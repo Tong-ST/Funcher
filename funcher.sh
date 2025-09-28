2202c40402f295bbdb54594cf75c79b0ca68aee4
Tong-ST <145736642+Tong-ST@users.noreply.github.com>
2025-09-28 12:43:58 +0700

    window focus after hidden

--- funcher.sh
+++ funcher.sh
@@ -247,6 +247,7 @@ anim_control () {
 			mpv_play_segment $START
 			sleep $DELAY_LAUNCHER_TIMER
 			window_ctrl "myapp" "$SCRATCHPAD_SHOW"
+			window_focus "myapp"
 			;;
 		"hide>idle")
 			window_ctrl "myapp" "$MOVE_TO_SCRATCHPAD"
@@ -254,6 +255,7 @@ anim_control () {
 			sleep $cooldown
 			mpv_play_segment $IDLE_START
 			window_ctrl "myapp" "$SCRATCHPAD_SHOW"
+			window_focus "myapp"
 			;;
 		*)
 			mpv_play_segment $start_segment
